79d58537ace30a9cf0fbe3360fc3e151cd88550f
psychonoob <psychonoob@gmx.ch>
2016-10-03 22:05:29 +0200

    Finish Uebung 2

--- Uebung2/Uebung2/Uebung2/ViewController.swift
+++ Uebung2/Uebung2/Uebung2/ViewController.swift
@@ -9,31 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var sliderlabel: UILabel!
     @IBOutlet weak var mySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var mySlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sliderlabel.text = String(mySlider.value)
         
-
-    
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var isShown: Bool = false
+    
     @IBAction func SilderValueChanged(_ sender: UISlider) {
+        
         self.sliderlabel.text = String(sender.value)
+        
+        if (sender.value >= 90 && self.isShown == false)
+        {
+            self.isShown = true
+            
+            let alertController = UIAlertController( title: "Information",
+                                                     message: "Ein Wert grösser als 90 angezeigt",
+                                                     preferredStyle: .alert)
+            let defaultAction = UIAlertAction(
+                title: "OK", style: .cancel,
+                handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+            
+        }
+        else if sender.value <= 90
+        {
+        self.isShown = false
+        }
+        
     }
     @IBAction func SegmentChanged(_ sender: UISegmentedControl) {
         
-        if sender.selectedSegmentIndex == 1 {
+        if sender.selectedSegmentIndex == 0 {
             self.view.backgroundColor = UIColor.white
         }else{
             self.view.backgroundColor = UIColor.gray
@@ -49,7 +74,20 @@ class ViewController: UIViewController {
         }
         
     }
+    @IBAction func infoButtonClicked(_ sender: UIButton) {
+        // var alert = UIAlertController(title:"")
+        let alertController = UIAlertController( title: "Information",
+                                                 message: "This is a message",
+                                                 preferredStyle: .alert)
+        let defaultAction = UIAlertAction(
+            title: "OK", style: .cancel,
+            handler: nil)
+        let otherAction = UIAlertAction(title:"...really",style:.default,handler:nil)
+        alertController.addAction(otherAction)
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
     
-
+    
 }
 
