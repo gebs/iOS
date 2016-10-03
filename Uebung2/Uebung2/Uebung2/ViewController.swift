a1a725e40c490fa8fd19bcfd2fd4896e85f14c4a
psychonoob <psychonoob@gmx.ch>
2016-10-03 20:55:16 +0200

    Beginn Implementation Uebung 2

--- Uebung2/Uebung2/Uebung2/ViewController.swift
+++ Uebung2/Uebung2/Uebung2/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var sliderlabel: UILabel!
+    @IBOutlet weak var mySpinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func SilderValueChanged(_ sender: UISlider) {
+        self.sliderlabel.text = String(sender.value)
+    }
+    @IBAction func SegmentChanged(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 1 {
+            self.view.backgroundColor = UIColor.white
+        }else{
+            self.view.backgroundColor = UIColor.gray
+        }
+    }
+    @IBAction func StartSpinner(_ sender: UIButton) {
+        if mySpinner.isAnimating{
+            mySpinner.stopAnimating()
+            sender.setTitle("Start Spinning", for: UIControlState.normal)
+        }else{
+            mySpinner.startAnimating()
+            sender.setTitle("Stop Spinning", for: UIControlState.normal)
+        }
+        
+    }
+    
 
 }
 
