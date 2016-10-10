c58a16f2ab856eb322c3fd11fcde717ff855a4f0
psychonoob <psychonoob@gmx.ch>
2016-10-10 20:44:12 +0200

    Init Commit Uebung 3

---
+++ Uebung3/Uebung3/Uebung3/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Uebung3
+//
+//  Created by iOSKurs on 10.10.16.
+//  Copyright © 2016 iOSKurs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var appearanceCounter = 0
+    @IBOutlet weak var lblApperanceCounter: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        appearanceCounter = appearanceCounter + 1
+        self.lblApperanceCounter.text = String(appearanceCounter) + ". Apperance"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func Show2ViewButtonPressed(_ sender: UIButton) {
+        let secondVC = SecondViewController()
+        secondVC.modalTransitionStyle = .partialCurl
+        self.present(secondVC,animated:true,completion: nil)
+        
+    }
+    @IBAction func back(segue: UIStoryboardSegue) { // ... 
+    }
+}
+
